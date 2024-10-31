d0c537b11a7def11a8ef6529b4ec5cff2e64591b
O009920 <mfuentes@forum.cl>
2024-10-31 14:02:37 -0300

    avance creacion componente listar libros

--- src/app/features/book/components/book-form/book-form.component.ts
+++ src/app/features/book/components/book-form/book-form.component.ts
@@ -1,32 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {BookService} from '../../../../core/services/book.service';
 import {Book} from '../../../../core/models/book.model';
+import {BookService} from '../../../../core/services/book.service';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-book-form',
+  templateUrl: './book-form.component.html',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatButton
   ],
-  templateUrl: './book-form.component.html',
-  styleUrl: './book-form.component.css'
+  styleUrls: ['./book-form.component.css']
 })
-export class BookFormComponent {
+export class BookFormComponent implements OnInit {
+  @Input() book: Book | null = null; // Si existe, es edición; si no, es nuevo
+  @Output() formSubmit = new EventEmitter<Book>();
   bookForm: FormGroup;
 
   constructor(private fb: FormBuilder, private bookService: BookService) {
     this.bookForm = this.fb.group({
       title: ['', Validators.required],
       author: ['', Validators.required],
+      gender: ['', Validators.required],
+      linkImage: [''],
       publishedDate: ['', Validators.required],
+      description: ['']
     });
+  }
+
+  ngOnInit(): void {
+    if (this.book) {
+      this.bookForm.patchValue(this.book); // Cargar datos en caso de edición
+    }
   }
 
   onSubmit(): void {
     if (this.bookForm.valid) {
-      const book: Book = this.bookForm.value;
-      this.bookService.createBook(book).subscribe();
+      const bookData: Book = { ...this.book, ...this.bookForm.value };
+      this.formSubmit.emit(bookData); // Emitir los datos del libro
+      this.bookForm.reset();
     }
   }
 }
--- src/app/features/book/components/book-list/book-list.component.ts
+++ src/app/features/book/components/book-list/book-list.component.ts
@@ -1,5 +1,13 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
+import { BookDetailDialogComponent } from '../book-detail-dialog/book-detail-dialog.component';
+import {Book} from '../../../../core/models/book.model';
+import {BookService} from '../../../../core/services/book.service';
+import {MatProgressSpinner} from '@angular/material/progress-spinner';
+import {FormsModule} from '@angular/forms';
+import {BookFormComponent} from '../book-form/book-form.component';
+import {NgForOf, NgIf} from '@angular/common';
 import {
   MatCard,
   MatCardActions,
@@ -8,36 +16,108 @@ import {
   MatCardImage, MatCardSubtitle,
   MatCardTitle
 } from '@angular/material/card';
-import {RouterLink} from '@angular/router';
-import {Book} from '../../../../core/models/book.model';
-import {BookService} from '../../../../core/services/book.service';
-import {MatAnchor, MatButton} from '@angular/material/button';
-import {NgForOf} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
-import {MatDialog, MatDialogModule} from '@angular/material/dialog';
-import {BookDetailDialogComponent} from '../book-detail-dialog/book-detail-dialog.component';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-book-list',
-  standalone: true,
   templateUrl: './book-list.component.html',
+  standalone: true,
   imports: [
+    MatProgressSpinner,
+    FormsModule,
+    BookFormComponent,
+    NgIf,
     MatCard,
     MatCardHeader,
-    MatCardTitle,
-    MatCardSubtitle,
-    MatCardContent,
-    MatCardActions,
-    RouterLink,
     MatCardImage,
-    MatAnchor,
     NgForOf,
+    MatCardContent,
     MatIcon,
-    MatDialogModule,
-    MatButton
+    MatCardActions,
+    MatButton,
+    MatCardTitle,
+    MatCardSubtitle
   ],
-  styleUrls: ['./book-list.component.scss']
+  styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent  {
+export class BookListComponent implements OnInit {
+  books: Book[] = [];
+  filteredBooks: Book[] = [];
+  searchTerm: string = '';
+  showForm: boolean = false;
+  selectedBook: Book | null = null;
+  loading: boolean = false;
 
+  constructor(private bookService: BookService, private dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.getBooks();
+  }
+
+  // Obtiene la lista de libros con un delay para ver el spinner
+  getBooks(): void {
+    this.loading = true;
+    this.bookService.getAllBooks().subscribe((data) => {
+      this.books = data;
+      this.filteredBooks = data;
+      setTimeout(() => {
+        this.loading = false; // Desactiva el spinner después de un delay
+      }, 500); // Delay de 500ms
+    });
+  }
+
+  openDetailDialog(book: Book): void {
+    this.dialog.open(BookDetailDialogComponent, {
+      width: '400px',
+      data: book
+    });
+  }
+
+  filterBooks(): void {
+    this.filteredBooks = this.books.filter((book) =>
+      book.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  openForm(): void {
+    this.selectedBook = null;
+    this.showForm = true;
+  }
+
+  editBook(book: Book): void {
+    this.selectedBook = book;
+    this.showForm = true;
+  }
+
+  onFormSubmit(book: Book): void {
+    this.loading = true;
+    if (book.id) {
+      this.bookService.updateBook(book).subscribe(() => {
+        this.getBooks();
+        this.showForm = false;
+        setTimeout(() => {
+          this.loading = false;
+        }, 500);
+      });
+    } else {
+      this.bookService.createBook(book).subscribe(() => {
+        this.getBooks();
+        this.showForm = false;
+        setTimeout(() => {
+          this.loading = false;
+        }, 500);
+      });
+    }
+  }
+
+  deleteBook(id: any): void {
+    this.loading = true;
+    this.bookService.deleteBook(id).subscribe(() => {
+      this.getBooks();
+      setTimeout(() => {
+        this.loading = false;
+      }, 500);
+    });
+  }
 }
